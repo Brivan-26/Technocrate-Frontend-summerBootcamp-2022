210a5d26d5d088814fdfe6ca609372fc0eb99d1d
Brivan-26 <ma.rezazi@esi-sba.dz>
2022-08-16 16:51:44 +0100

    Session3

---
+++ session3-corrupted/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+  
+    extend: {
+      colors: {
+        'myPuprle': '#623CEA'
+      }
+    },
+  },
+  plugins: [],
+};
